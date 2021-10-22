e45e668329a5997ad0eda35904c30cb9bcfca73c
MrRobot420 <max.karlchen@web.de>
2021-10-22 18:31:45 +0200

    change according to formatting and data

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const fs = require('fs')
 var csv = require("csvtojson");
-const csvFilePath = "./data/lifehacks_de.csv"
-const outFilePath = "./data/lifehacks_de.json"
+const csvFilePath = "./data/cybertrez420-all-txs.csv"
+const outFilePath = "./data/cybertrez420-all-txs.json"
  
 // Convert a csv file with csvtojson
-csv({ delimiter: ';'})
+csv({ delimiter: ','})
   .fromFile(csvFilePath)
   .then(lifehacks => { //when parse finished, result will be emitted here.
     lifehacks = lifehacks.map(lifehack => {
-        delete lifehack.imageNumber
-        lifehack.isFavorite = false;
-        lifehack.isFeatured = false;
         return lifehack
     })
     console.log(lifehacks);
